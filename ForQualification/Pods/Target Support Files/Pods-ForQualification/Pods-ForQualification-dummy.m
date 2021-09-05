dce6f8fdb3ecfd4e0792f9f39c423584b51533e2
kunihei <57933493+kunihei@users.noreply.github.com>
2021-09-05 09:26:18 +0900

    podFileの追加

---
+++ ForQualification/Pods/Target Support Files/Pods-ForQualification/Pods-ForQualification-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForQualification : NSObject
+@end
+@implementation PodsDummy_Pods_ForQualification
+@end
---
+++ ForQualification/Pods/Target Support Files/Pods-ForQualificationTests/Pods-ForQualificationTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForQualificationTests : NSObject
+@end
+@implementation PodsDummy_Pods_ForQualificationTests
+@end
