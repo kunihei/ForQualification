73360bd06ee74cb298df22653f8d31bbdfd36d39
kunihei <57933493+kunihei@users.noreply.github.com>
2021-09-28 22:22:03 +0900

    次の問題に進める機能と解答結果の表示、問題数の表示

--- ForQualification/ForQualification/Controller/ChallengeProblem.swift
+++ ForQualification/ForQualification/Controller/ChallengeProblem.swift
@@ -11,12 +11,15 @@ class ChallengeProblem: UIViewController {
 
     @IBOutlet weak var selectTextView: UITextView!
     @IBOutlet weak var answerLabel: UILabel!
-    @IBOutlet weak var answerNextButton: UIButton!
+    @IBOutlet weak var answerButton: UIButton!
+    @IBOutlet weak var problemCountLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
     
     private let getSelectList = GetProblemSelect()
     private let getProblemAnswerList = GetProblem_Answer()
     
     var pickerView = UIPickerView()
+    private var lastFlag = false
     private var problemCount = 0
     
     override func viewDidLoad() {
@@ -26,23 +29,29 @@ class ChallengeProblem: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        pickerView.delegate = self
-        pickerView.dataSource = self
         selectTextView.isEditable = false
-        selectTextView.text = "ここをタッチして正解を選んでください！"
-        answerNextButton.setTitle("決定", for: .normal)
-        answerNextButton.isEnabled = false
+        nextButton.isHidden = true
         pickerView.selectRow(0, inComponent: 0, animated: true)
         getProblemAnswerList.getProblemList()
         getSelectList.getProblemSelect()
         problemCount = 0
+        initialValue()
         getSelectList.problemSelectEmptyDelete = []
         getProblemAnswerList.problemList = []
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        pickerView.delegate = self
+        pickerView.dataSource = self
         doneBar()
         getSelectList.selectEmptyDelete(problemCount: problemCount)
+    }
+    
+    func initialValue() {
+        problemCountLabel.text = "第\(problemCount + 1)問"
+        selectTextView.text = "ここをタッチして正解を選んでください！"
+        answerLabel.text = "結果"
+
     }
     
     func doneBar() {
@@ -58,21 +67,70 @@ class ChallengeProblem: UIViewController {
     @objc func done() {
         selectTextView.endEditing(true)
         selectTextView.text = "\(getSelectList.problemSelectEmptyDelete[pickerView.selectedRow(inComponent: 0)])"
-        answerNextButton.isEnabled = true
+    }
+    
+    //pickerViewの表示を消す
+    func notPickerView(){
+        selectTextView.inputView = .none
+        selectTextView.inputAccessoryView = .none
+        selectTextView.isEditable = false
     }
 
-    @IBAction func answerNextButton(_ sender: Any) {
+    @IBAction func answerButton(_ sender: Any) {
+        errAlert()
+        
+        if problemCount == getProblemAnswerList.problemList.count - 1 {
+            nextButton.setTitle("採点画面へ", for: .normal)
+            lastFlag = true
+        }
+        
+        checkTheAnswer()
+        notPickerView()
+        nextButton.isHidden = false
+        answerButton.isEnabled = false
+    }
+    
+    func checkTheAnswer() {
         if selectTextView.text == getProblemAnswerList.problemList[problemCount].answer {
             answerLabel.text = "正解"
         } else {
             answerLabel.text = "正解は「\(getProblemAnswerList.problemList[problemCount].answer)」"
         }
-        answerNextButton.setTitle("次の問題へ", for: .normal)
-    }
-    @IBAction func backButton(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
     }
     
+    func errAlert() {
+        if selectTextView.text == "ここをタッチして正解を選んでください！" {
+            let alert: UIAlertController = UIAlertController(title: "未選択", message: "選択肢から答えを選んで下さい！", preferredStyle: UIAlertController.Style.alert)
+            let confirmAction: UIAlertAction = UIAlertAction(title: "OK", style: .cancel)
+            alert.addAction(confirmAction)
+            present(alert, animated: true, completion: nil)
+            return
+        }
+    }
+    
+    func nextProblem() {
+        if problemCount < getProblemAnswerList.problemList.count - 1 {
+            problemCount += 1
+            initialValue()
+            getSelectList.selectEmptyDelete(problemCount: problemCount)
+            doneBar()
+            pickerView.selectRow(0, inComponent: 0, animated: true)
+        }
+    }
+    
+    func lastProblem() {
+        if lastFlag {
+            let resultView = ResultProblem()
+            navigationController?.pushViewController(resultView, animated: true)
+        }
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        nextProblem()
+        nextButton.isHidden = true
+        answerButton.isEnabled = true
+        lastProblem()
+    }
     /*
     // MARK: - Navigation
 
